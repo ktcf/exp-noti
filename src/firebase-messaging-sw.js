1b8c1c95074c807f25c124b1f1c04b12258a9d6d
ktcf <5641897+ktcf@users.noreply.github.com>
2022-11-15 21:03:48 +0200

    add manifest.json and service worker

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,9 @@
+importScripts("https://www.gstatic.com/firebasejs/9.14.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging-compat.js");firebase.initializeApp({
+  apiKey: "AIzaSyDv14AFHlP3WS_RoRS58NFFguF08OdSwdA",
+  projectId: "test-c21ec",
+  messagingSenderId: "318695599071",
+  appId: "1:318695599071:web:ec2cde5e6184ce7112b26a",
+});
+
+const messaging = firebase.messaging();
