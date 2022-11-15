064ec5d9e03679b85d37966364a6e3348259b770
ktcf <5641897+ktcf@users.noreply.github.com>
2022-11-15 21:05:46 +0200

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDv14AFHlP3WS_RoRS58NFFguF08OdSwdA",
+    projectId: "test-c21ec",
+    messagingSenderId: "318695599071",
+    appId: "1:318695599071:web:ec2cde5e6184ce7112b26a",
+    vapidKey: "BAYoMYHynGHC61uSO5LHuOVGjSbnjd9E_R5Yhq1lWr5UH59NdHPfgw7WCSa2avb0Ql-dwjVRd0NR-MNdGCOLHII"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDv14AFHlP3WS_RoRS58NFFguF08OdSwdA",
+    projectId: "test-c21ec",
+    messagingSenderId: "318695599071",
+    appId: "1:318695599071:web:ec2cde5e6184ce7112b26a",
+    vapidKey: "BAYoMYHynGHC61uSO5LHuOVGjSbnjd9E_R5Yhq1lWr5UH59NdHPfgw7WCSa2avb0Ql-dwjVRd0NR-MNdGCOLHII"
+  }
 };
 
 /*
